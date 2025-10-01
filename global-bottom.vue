5342bc6828580a359cbfd44c8b4590db390fddb6
Your Name <you@example.com>
2025-10-01 23:18:57 +0900

    feat: add global templates

---
+++ global-bottom.vue
@@ -0,0 +1,7 @@
+<!-- global-bottom.vue -->
+<template>
+  <div class="text-center text-white absolute bottom-0 right-10 p-2 z-[9999] bg-[#5C6778] h-[40px] w-[60px]">
+    {{ $nav.currentPage }}
+  </div>
+  <div class="absolute bottom-0 w-full h-[20px] bg-[#9A003D]">&nbsp;</div>
+</template>
---
+++ global-top.vue
@@ -0,0 +1,9 @@
+<!-- global-top.vue -->
+<template>
+  <div v-if="$nav.currentPage === 1"
+    class="absolute top-0 left-10 w-[100px] h-[100px] bg-[url('assets/logo.png')] bg-cover bg-center"
+  ></div>
+  <span class="absolute top-5 right-10 right-0 color-[#9A003D]">
+    SOPHIA UNIVERSITY
+  </span>
+</template>
